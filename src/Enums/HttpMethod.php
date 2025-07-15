c89fe05122c755d97121c6fea4e40a9de0515c56
henrywood <henry.wood.dk@gmail.com>
2025-07-15 16:45:47 +0200

    Create HttpMethod.php

---
+++ src/Enums/HttpMethod.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Elysian\\DocumentationAttributes\\Enums;
+
+enum HttpMethod: string {
+    case GET = 'GET';
+    case POST = 'POST';
+    case PUT = 'PUT';
+    case DELETE = 'DELETE';
+    case PATCH = 'PATCH';
+    case OPTIONS = 'OPTIONS';
+}
